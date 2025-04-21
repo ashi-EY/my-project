14d9b2f81d22b81d9af4b9f68c27ec34ccd8a81a
Ashi Jain <ashi.jain2@in.ey.com>
2025-04-21 12:11:41 +0530

    fix code smell

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,15 +2,17 @@ package com.example.demo.controller;
 
 import com.example.demo.model.HelloEntity;
 import com.example.demo.repository.HelloRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @Autowired
-    private HelloRepository helloRepository;
+    private final HelloRepository helloRepository;
+
+    public HelloController(HelloRepository helloRepository) {
+        this.helloRepository = helloRepository;
+    }
 
     @GetMapping("/hello")
     public String getHelloWorld() {
